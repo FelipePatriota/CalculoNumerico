11148cb35ef283792ef377a4df584c7fc0738201
FelipePatriota <felipe.patriota@ufrpe.br>
2022-12-10 15:26:59 -0300

    #método do ponto fixo com comentarios

--- Metodos/pontofixo.py
+++ Metodos/pontofixo.py
@@ -1,21 +1,21 @@
 #método do ponto fixo
 
-import math
-import time
+import math # importa a biblioteca math
+import time # importa a biblioteca time
 
 def f(x):
-  return math.pow(x,3) + math.pow(x,2) - x + 1
+  return math.pow(x,3) + math.pow(x,2) - x + 1 #função a ser analisada
 def phi(x):
-  return math.pow(-x,2) - x + 1
-a = -1 #intervalo
-e = 0.00001
-k = 0
+  return math.pow(-x,2) - x + 1 #função phi
+a = -1 #intervalo inicial
+e = 0.00001 #precisão
+k = 0 #iterações iniciais em 0
 
 x = a
-while abs(f(x)) > e: #loop
-  x = phi(x)
-  print(x)
-  k = k + 1
+while abs(f(x)) > e: #loop para encontrar a raiz
+  x = phi(x) #atualiza o ponto inicial
+  print(x) #imprime o ponto inicial
+  k = k + 1 #atualiza o número de iterações
  
-print("A raiz encontrada foi: ", x)
-print("O número de iteraçõesfoi: ",k)
+print("A raiz encontrada foi: ", x) #imprime a raiz
+print("O número de iteraçõesfoi: ",k) #imprime o número de iterações
