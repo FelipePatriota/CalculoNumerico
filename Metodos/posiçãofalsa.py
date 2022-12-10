ad283af2cabe79d1260e292321b7c307d0ea336f
FelipePatriota <felipe.patriota@ufrpe.br>
2022-12-10 15:28:22 -0300

    #Método da posição falsa com comentarios

--- Metodos/posiçãofalsa.py
+++ Metodos/posiçãofalsa.py
@@ -1,24 +1,23 @@
 #Método da posição falsa
-import math
+
+import math # importa a biblioteca math
 def f(x):
-  return math.pow(x,3) + math.pow(x,2) - x + 1
+  return math.pow(x,3) + math.pow(x,2) - x + 1 #função a ser analisada
 
-k = 0
-a = -1
-b = -2
-e = 0.00001
+k = 0 #iterações iniciais em 0
+a = -1 #intervalo inicial
+b = -2 #intervalo final
+e = 0.00001 #precisão
 
-m = (a+b)/2
-while abs(f(m) > e):
-  m = (a*f(b) - b*f(a))/ (f(b) - f(a))
-  if f(a)*f(m) < 0:
-    b = m
-  else:
-    a = m
-  k = k + 1
+m = (a+b)/2 #ponto médio
 
- 
+while abs(f(m) > e): #enquanto o intervalo for maior que a precisão
+  m = (a*f(b) - b*f(a))/ (f(b) - f(a)) #calcula o ponto médio
+  if f(a)*f(m) < 0: #se o produto for menor que 0, a raiz está no intervalo [a,m]
+    b = m #atualiza o intervalo
+  else: #se não, a raiz está no intervalo [m,b]
+    a = m #atualiza o intervalo
+  k = k + 1 #atualiza o número de iterações
 
-  
-print("A raiz encontrada foi: ", m)
-print("O número de iterações foi: ",k)
+print("A raiz encontrada foi: ", m) #imprime a raiz
+print("O número de iterações foi: ",k) #imprime o número de iterações
