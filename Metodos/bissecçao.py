51a62316cc7471e34990a46ab9c6eff475a0939e
FelipePatriota <felipe.patriota@ufrpe.br>
2023-01-07 19:42:17 -0300

    Atualização

--- Metodos/bissecçao.py
+++ Metodos/bissecçao.py
@@ -7,7 +7,7 @@ def f(x):
 
 k = 0  #iterações iniciais em 0 
 a = -2 #intervalo inicial
-b = -1 #intervalo final 
+b = -1 #intervalo final  
 e = 0.00001 #precisão 
 
 if f((a+b)/2) < e: #se a raiz for encontrada no meio do intervalo
@@ -17,7 +17,7 @@ else: #se não, inicia o método da bissecção
   while b - a > e: #enquanto o intervalo for maior que a precisão
     m = (a + b)/2 #calcula o ponto médio
     if f(a)*f(m) < 0: #se o produto for menor que 0, a raiz está no intervalo [a,m]
-      b = m #atualiza o intervalo
+      b = m #atualiza o intervalo 
     else: #se não, a raiz está no intervalo [m,b]
       a = m #atualiza o intervalo
     k = k + 1 #atualiza o número de iterações
