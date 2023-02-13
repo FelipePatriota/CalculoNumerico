3e47f0be62c95ee9a260363ca4c36f71fffbd399
Felipe Patriota <felipe.patriota@ufrpe.br>
2023-02-13 14:44:05 -0300

    att valores

--- InterpolaçãoPolinomial/InterpolacaoPolinomial.py
+++ InterpolaçãoPolinomial/InterpolacaoPolinomial.py
@@ -13,8 +13,8 @@ def interpolation_polynomial(x_values, y_values, x):
         result += coefficient * x**(n-i-1)
     return result
 
-x_values = [1, 2, 3]
-y_values = [2, 4, 6]
+x_values = [20, 25, 30, 35, 40, 45, 50]
+y_values = [0.99907, 0.99852, 0.99826, 0.99818, 0.99828, 0.99849, 0.99878]
 x = 4
 result = interpolation_polynomial(x_values, y_values, x)
 print(result)
