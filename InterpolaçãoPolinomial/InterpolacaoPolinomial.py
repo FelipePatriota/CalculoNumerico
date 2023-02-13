4ae8d983e26e1fca0ec25663e6b6e8645cd5f5d6
Felipe Patriota <felipe.patriota@ufrpe.br>
2023-02-13 15:21:32 -0300

    Att

--- InterpolaçãoPolinomial/InterpolacaoPolinomial.py
+++ InterpolaçãoPolinomial/InterpolacaoPolinomial.py
@@ -1,20 +1,26 @@
 import numpy as np
 
-def interpolation_polynomial(x_values, y_values, x):
-    n = len(x_values)
-    A = np.zeros((n, n))
-    for i in range(n):
-        for j in range(n):
-            A[i][j] = x_values[i]**(n-j-1)
-    b = y_values
-    coefficients = np.linalg.solve(A, b)
-    result = 0
-    for i, coefficient in enumerate(coefficients):
-        result += coefficient * x**(n-i-1)
-    return result
+# Tabela de valores experimentais de calor específico
+temperaturas = np.array([20, 25, 30, 35, 40, 45, 50])
+calor_especifico = np.array([0.99907, 0.99852, 0.99826, 0.99818, 0.99828, 0.99849, 0.99878])
 
-x_values = [20, 25, 30, 35, 40, 45, 50]
-y_values = [0.99907, 0.99852, 0.99826, 0.99818, 0.99828, 0.99849, 0.99878]
-x = 4
-result = interpolation_polynomial(x_values, y_values, x)
-print(result)
+# Função para interpolação polinomial usando eliminação gaussiana
+def interpola_polinomial(x, y, x_val):
+    n = len(x)
+    A = np.zeros((n, n))
+    b = np.zeros(n)
+    for i in range(n):
+        b[i] = y[i]
+        for j in range(n):
+            A[i, j] = x[i] ** (n - j - 1)
+    # Resolução do sistema por eliminação gaussiana
+    x = np.linalg.solve(A, b)
+    y_val = 0
+    for i in range(n):
+        y_val += x[i] * x_val ** (n - i - 1)
+    return y_val
+
+# Cálculo do polinômio de grau 2
+x_val = 32.5
+p2 = interpola_polinomial(temperaturas, calor_especifico, x_val)
+print("Polinômio de grau 2:", p2)
