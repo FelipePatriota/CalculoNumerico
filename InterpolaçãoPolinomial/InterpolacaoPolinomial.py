0f6cb96abd1a82a160c3b1d299c9fda893836e50
FelipePatriota <felipe.patriota@ufrpe.br>
2023-03-03 18:32:17 -0300

    a interpolação polinomial

--- InterpolaçãoPolinomial/InterpolacaoPolinomial.py
+++ InterpolaçãoPolinomial/InterpolacaoPolinomial.py
@@ -1,26 +1,41 @@
 import numpy as np
 
-# Tabela de valores experimentais de calor específico
-temperaturas = np.array([20, 25, 30, 35, 40, 45, 50])
-calor_especifico = np.array([0.99907, 0.99852, 0.99826, 0.99818, 0.99828, 0.99849, 0.99878])
+# Essa função calcula o valor de um polinômio para um determinado valor de x, dado seus coeficientes armazenados em uma lista.
+def p(x, a):
+    r = 0
+    n = len(a)
+    for i in range(0, n):
+        r += a[i] * (x**i)
+    return r
 
-# Função para interpolação polinomial usando eliminação gaussiana
-def interpola_polinomial(x, y, x_val):
-    n = len(x)
-    A = np.zeros((n, n))
-    b = np.zeros(n)
-    for i in range(n):
-        b[i] = y[i]
-        for j in range(n):
-            A[i, j] = x[i] ** (n - j - 1)
-    # Resolução do sistema por eliminação gaussiana
-    x = np.linalg.solve(A, b)
-    y_val = 0
-    for i in range(n):
-        y_val += x[i] * x_val ** (n - i - 1)
-    return y_val
+# Nesses trechos, são definidos os conjuntos de pontos para os quais serão ajustados polinômios.
+x = [30, 35, 40]
+y = [0.99826, 0.99818, 0.99828]
 
-# Cálculo do polinômio de grau 2
-x_val = 32.5
-p2 = interpola_polinomial(temperaturas, calor_especifico, x_val)
-print("Polinômio de grau 2:", p2)
+w = [20, 25, 30, 35, 40, 45, 50]
+z = [0.99907, 0.99852, 0.99826, 0.99818, 0.99828, 0.99849, 0.99878]
+
+# Essa função retorna a matriz de Vandermonde para um conjunto de pontos x, que é utilizada para ajustar um polinômio aos pontos.
+def vandermonde_matrix(x, n):
+    V = np.zeros((len(x), n+1))
+    for i in range(len(x)):
+        for j in range(n+1):
+            V[i,j] = x[i]**j
+    return V 
+
+# Esses são os graus dos polinômios que serão ajustados
+grau_p2 = 2
+grau_p6 = 6
+
+# Matrizes de Vandermonde para cada conjunto de pontos
+V = vandermonde_matrix(x, grau_p2)
+D = vandermonde_matrix(w, grau_p6)
+
+# Resolução do sistema para os coeficientes dos polinômios
+a = np.linalg.solve(V, y)
+c = np.linalg.solve(D, z)
+
+
+# Impressão dos resultados
+print("Esse é o valor para um polinômio de grau 2:", p(32.5, a))
+print("Esse é o valor para um polinômio de grau 6:", p(32.5, c))
