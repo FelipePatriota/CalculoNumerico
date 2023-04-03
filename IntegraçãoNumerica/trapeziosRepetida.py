41685b71dc39060d04c908e916448c15ece1f05c
FelipePatriota <felipe.patriota@ufrpe.br>
2023-04-03 11:36:21 -0300

    ResultadoManual

--- IntegraçãoNumerica/trapeziosRepetida.py
+++ IntegraçãoNumerica/trapeziosRepetida.py
@@ -6,7 +6,7 @@ def f(x):
 
 
 #Usando a regra do trapezio repetido para que tenhap a aproximacao da integral no intervalo [a,b]
-def trapezoidal_rule(f, a, b, n):
+def trapezoidalRepetido(f, a, b, n):
     h = (b - a) / n  #Subintervalos
     x = [a + i * h for i in range(n + 1)] # pontos x_i
     y = [f(x_i) for x_i in x]   # pontos y_i
@@ -19,4 +19,29 @@ if __name__ == '__main__':
     a = 0
     b = 1
     n = 41
-    print(trapezoidal_rule(f, a, b, n))
+    print(trapezoidalRepetido(f, a, b, n))
+
+
+
+
+
+#---------------------Resultado---------------------#
+# calculo do erro 
+# erro = (b-a)**3/(12*n**2)*integral
+# erro = (1-0)**3/(12*41**2)*0.7468241328124271
+# erro = 0.0001
+
+#-------------------- Resultado --------------------#
+#calculo integral
+# integral = (b-a)/2*(f(a)+f(b))
+# integral = (1-0)/2*(math.exp(-0**2)+math.exp(-1**2))
+# integral = 0.7468241328124271
+
+#---------------------Resultado---------------------#
+# calculo para obter o numero de intervalos n 
+# a formula eh n = sqrt(2*erro/(b-a)*integral)
+# n = sqrt(2*0.0001/(1-0)*0.7468241328124271)
+# n = 41.0
+
+
+
